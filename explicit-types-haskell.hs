9b735ed615dcb63b107db2de6e2ba03fd9d2735f
Beregszászi István <beistvan@gmail.com>
2023-11-02 08:04:37 +0100

    Explicit Types In Haskell

---
+++ explicit-types-haskell.hs
@@ -0,0 +1,24 @@
+main = do
+    let str :: [Char]
+        str = "hello"
+    putStrLn str
+
+    let c :: Char
+        c = 'a'
+    print $ c
+    
+    let i :: Int
+        i = 1
+    print $ i
+    
+    let d :: Double
+        d = 1.2
+    print $ d
+    
+    let b :: Bool
+        b = True
+    print $ b
+    
+    let arr :: [Int]
+        arr = [1, 2, 3]
+    print $ arr
